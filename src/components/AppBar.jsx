57db8144e6cc9fa415e07f49353d7fcd10ef5ed1
W-oland <tatu.jarvenranta@gmail.com>
2022-01-10 12:57:31 +0200

    tehtavat 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, Pressable, Alert } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 
 import Text from './Text';
@@ -9,6 +10,9 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: Constants.statusBarHeight,
         backgroundColor: theme.backgroundColor.appBar,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     text: {
         color: theme.colors.textAppBar,
@@ -16,17 +20,24 @@ const styles = StyleSheet.create({
 });
 
 const Tab = (props) => {
+    //Alert.alert('you pressed vladimir!')}
     return (
-        <Pressable onPress={() => Alert.alert('you pressed vladimir!')}>
-            <Text style={{ paddingBottom: 40, paddingTop: 40, paddingLeft: 140}} color="textAppBar">{props.name}</Text>
-        </Pressable>
+        <Link to={props.link}>
+            <Text style={{ paddingBottom: 40, paddingTop: 40, paddingLeft: 40, paddingRight: 40}} color="textAppBar">{props.name}</Text>
+            {/*<Pressable onPress={() => console.log(props?.message)}>
+                <Text style={{ paddingBottom: 40, paddingTop: 40, paddingLeft: 40, paddingRight: 40}} color="textAppBar">{props.name}</Text>
+            </Pressable>*/}
+        </Link>
     );
 };
 
 const AppBar = () => {
     return (
     <View style={styles.container}>
-        { Tab({ name: 'Repositories' }) }
+        <ScrollView horizontal>
+            { Tab({ name: 'Sign-in', link:"/signin", message:'signin call' }) }
+            { Tab({ name: 'Repositories', link:"/", message: 'repositories call' }) }
+        </ScrollView>
     </View>
     );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native'
 
 import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
 import AppBar from './AppBar';
 import theme from '../theme';
 
@@ -19,7 +21,15 @@ const Main = () => {
   return (
     <View style={styles.flexContainer}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+      <Route path="/signin" exact>
+          <SignIn />
+        </Route>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
