05367c0733df9b558fdf61f9c84cf23f6bc04a8a
W-oland <tatu.jarvenranta@gmail.com>
2022-03-07 07:43:22 +0200

    tehtava 10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import 'dotenv/config';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.1.100:4000/graphql',
+  uri: Constants.manifest.extra.uri //'http://192.168.1.100:4000/graphql',
 });
 
 const createApolloClient = () => {
